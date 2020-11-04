1e45c81d0177a5c0b91e51cf8577244362041e38
홍유진 <eugene@hong-yujin-ui-MacBook-Pro.local>
2020-11-04 21:47:57 +0900

    쉬운문제
    2442 - 별찍기 - 5
    2446 - 별찍기 - 9
    10872 - 팩토리얼

---
+++ Implementation/2442.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+#for i in range(n-1, 0, -1):
+#  print(" " * ((n-1) - i), end="")
+#  print("*" * i * 2,end="")
+#  print("*")
+
+print(" " * (n-1), end="")
+print("*")
+
+for i in range(1, n):
+  print(" " * ((n-1) - i), end="")
+  print("*" * i * 2,end="")
+  print("*")
