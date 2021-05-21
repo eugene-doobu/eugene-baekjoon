d013900af83dcc6e065f6c952500c8deb36e4628
eugene-doobu <ssundong0_0@naver.com>
2021-05-21 14:38:28 +0900

    add 68645.py

---
+++ python3/_programmers/implementation/68645.py
@@ -0,0 +1,41 @@
+def solution(n):
+    array = [[0] * n for _ in range(n)]
+
+    maximum = 0
+    for i in range(1,n+1):
+      maximum += i
+
+    row = -1
+    col = 0
+    power = n
+    qwe = 1
+    while power >= 1:
+      x = 0
+      y = 0
+      if (n-power) % 3 == 0:
+        x = 0
+        y = 1
+      elif (n-power) % 3 == 1:
+        x = 1
+        y = 0
+      elif (n-power) % 3 == 2:
+        x = -1
+        y = -1
+
+      col += x
+      row += y
+      for k in range(power):
+        array[row][col] = qwe
+        qwe += 1
+        col += x
+        row += y
+      col -= x
+      row -= y
+      power -=1
+
+    rst = []  
+    for k in array:
+      for l in k:
+        if l != 0:
+          rst.append(l)
+    return rst
