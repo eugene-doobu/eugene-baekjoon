9eed14794a40f5a06bb859d2a5fa5155f8f91b13
eugene-doobu <ssundong0_0@naver.com>
2021-05-29 20:22:08 +0900

    1700

--- nodejs/bojs/Greedy/1700.js
+++ nodejs/bojs/Greedy/1700.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 const stdin = (process.platform === 'linux'
         ? fs.readFileSync('/dev/stdin').toString()
-        : `3 14
-1 4 3 2 5 4 3 2 5 3 4 2 3 4
-`
+        : `3 6
+1 1 1 1 2 3`
 ).split('\n');
 
 const fLine = stdin[0].split(' ').map(Number);
@@ -14,21 +13,25 @@ const K = fLine[1];
 let tap = [];
 let rst = 0;
 
-for(let i=0; i<N; i++){
-    tap.push(sLine[i]);
+let count = 0;
+for(let i=0; count<N; i++){
+    if(tap.indexOf(sLine[i]) == -1){
+        tap.push(sLine[i]);
+        count++;
+    }
 }
-tap.sort(function (a, b){
-    return a-b;
-})
 
 for(let i=N; i<K; i++){
-    if(!tap.includes(sLine[i])){
-        tap.pop()
-        tap.push(sLine[i])
-        tap.sort(function (a, b){
-            return a-b;
-        })
-        rst++;
-    }
+    if(tap.includes(sLine[i])) continue;
+
+    // 후에 사용순서 탐색
+    let tmp = [];
+    tap.forEach((e)=>{
+        let idx = sLine.indexOf(e, i+1);
+        tmp.push(idx==-1?10001:idx);
+    })
+    let maxVal = Math.max.apply(null, tmp);
+    tap[tmp.indexOf(maxVal)] = sLine[i];
+    rst++;
 }
 console.log(rst);
