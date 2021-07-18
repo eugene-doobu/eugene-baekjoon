13afd111b291872ef2556399e88f1d506aa8d351
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2021-07-18 21:37:45 +0900

    Create 9237.cpp

---
+++ cpp/boj/greedy-easy/9237.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n=0, m[100001], rst = -1;
+	scanf("%d", &n); for (int i = 0; i < n; i++) scanf("%d", m + i);
+	sort(m, m + n);
+	for (int i = 0; i < n; i++) rst = max(rst, m[i]+n-i);
+	printf("%d",rst + 1);
+}
