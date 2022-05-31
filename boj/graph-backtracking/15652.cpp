2ef22ef12aee08bf97cda44f7732c17ee258a68e
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2022-05-31 09:49:25 +0900

    Create 15652.cpp

---
+++ boj/graph-backtracking/15652.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int n, m;
+int arr[9];
+bool visited[9];
+
+void dfs(int cnt)
+{
+    if (cnt == m)
+    {
+        for (int i = 0; i < m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (cnt > 0 && arr[cnt - 1] > i) continue;
+        arr[cnt] = i;
+        dfs(cnt + 1);
+    }
+}
+
+int main(void) {
+	fastio;
+    cin >> n >> m;
+    dfs(0);
+}
+
+
+
