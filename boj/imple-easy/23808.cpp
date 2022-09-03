f7bf554f3e8efe1569c732e3dce08970ddbabd26
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2022-09-03 23:23:41 +0900

    Create 23808.cpp

---
+++ boj/imple-easy/23808.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio cin.tie(0)->sync_with_stdio(0)
+
+int main()
+{
+	fastio;
+	int n; cin >> n;
+	for (int i = 0; i < n * 2; ++i) {
+		for (int j = 0; j < n * 1; ++j) cout << "@";
+		for (int j = 0; j < n * 3; ++j) cout << " ";
+		for (int j = 0; j < n * 1; ++j) cout << "@";
+		cout << "\n";
+	}
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n * 5; ++j) cout << "@";
+		cout << "\n";
+	}
+	for (int i = 0; i < n * 1; ++i) {
+		for (int j = 0; j < n * 1; ++j) cout << "@";
+		for (int j = 0; j < n * 3; ++j) cout << " ";
+		for (int j = 0; j < n * 1; ++j) cout << "@";
+		cout << "\n";
+	}
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n * 5; ++j) cout << "@";
+		cout << "\n";
+	}
+}
