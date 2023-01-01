783d666abd5abe8aad03fe41cac005b44689506d
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2023-01-01 21:35:53 +0900

    Create 26766.cpp

---
+++ boj/imple-easy/26766.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		cout << " @@@   @@@ " << "\n";
+		cout << "@   @ @   @" << "\n";
+		cout << "@    @    @" << "\n";
+		cout << "@         @" << "\n";
+		cout << " @       @ " << "\n";
+		cout << "  @     @  " << "\n";
+		cout << "   @   @   " << "\n";
+		cout << "    @ @    " << "\n";
+		cout << "     @     " << "\n";
+	}
+}
