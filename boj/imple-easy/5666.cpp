2ca10b5fe285241b222964ebac7c17f7c2a0b1b7
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2023-05-19 00:01:51 +0900

    Create 5666.cpp

---
+++ boj/imple-easy/5666.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+int main() {
+	double h, p;
+	while (cin >> h) {
+		cin >> p;
+		printf("%.2f\n", h / p);
+	}
+}
+
