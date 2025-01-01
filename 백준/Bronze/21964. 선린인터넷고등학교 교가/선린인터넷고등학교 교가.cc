bd1c8fdf94114bd34f677fca2352bd00d4ec2ccb
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-01-01 14:28:46 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.cc
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+int main() {
+    fastio; int n; string s; cin >> n >> s;
+    for (int i = n - 5; i < n; ++i) cout << s[i];
+}
