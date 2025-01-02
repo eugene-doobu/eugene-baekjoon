de8f927756b11b24053538069d98100ce3f213ec
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-01-02 16:52:08 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+int main() {
+    fastio; char c; cin >> c;
+    if (c == 'M') cout << "MatKor";
+    if (c == 'W') cout << "WiCys";
+    if (c == 'C') cout << "CyKor";
+    if (c == 'A') cout << "AlKor";
+    if (c == '$') cout << "$clear";
+}
