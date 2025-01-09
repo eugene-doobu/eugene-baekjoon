c360a34a15e42bfb8d57373ec3702c185f74ad81
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-01-09 11:58:41 +0900

    [Bronze IV] Title: Greetings!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17548. Greetings！/Greetings！.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+int main() {
+    fastio; string s; cin >> s;
+    cout << "h";
+    int t = (s.length() - 2) * 2;
+    while (t--) cout << 'e';
+    cout << 'y';
+}
