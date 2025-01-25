3655fa9fd255bcec3d67a28e58b6bcff86a75038
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-01-26 03:48:56 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+int main() {
+	fastio; double a, b; cin >> a >> b;
+	cout << fixed;
+	cout.precision(1);
+	cout << a * b / 2;
+}
