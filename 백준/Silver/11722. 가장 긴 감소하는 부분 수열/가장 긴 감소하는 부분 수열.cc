2f6824572b954ecf461974d8f7b5a825366e8a9d
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-02-03 23:45:38 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int dp[1001];
+int vals[1001];
+
+int main() {
+	fastio; int n, mx=1; cin >> n;
+	for (int i = 1; i <= n; ++i) cin >> vals[i];
+	for (int i=1; i<=n; ++i)
+	{
+		dp[i] = max(dp[i], 1);
+		for (int j=i+1; j<=n; ++j)
+		{
+			if (vals[i] > vals[j])
+			{
+				dp[j] = max(dp[j], dp[i] + 1);
+				mx = max(mx, dp[j]);
+			}
+		}
+	}
+	cout << mx;
+}
