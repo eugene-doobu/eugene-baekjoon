4e59bf59275a681146d44adeab4b588993d3a22b
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-02-25 00:04:50 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+int main() {
+    int a, b, c; cin >> a >> b >> c;
+    if (a + b == c) cout << "correct!\n";
+    else cout << "wrong!\n";
+}
