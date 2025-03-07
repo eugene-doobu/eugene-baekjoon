26a9a9ed48b4b96295bee7deacc22014571c2b88
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-03-07 14:06:34 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -10,13 +10,9 @@ int solve(int a, int b, int c) {
     if (a > 20 || b > 20 || c > 20)
         return solve(20, 20, 20);
     if (dp[a][b][c] != 0x7f7f7f7f) return dp[a][b][c];
-    if (a < b && b < c) {
-        dp[a][b][c] = solve(a, b, c - 1) + solve(a, b - 1, c - 1) - solve(a, b - 1, c);
-        return dp[a][b][c];
-    }
-
-    dp[a][b][c] = solve(a - 1, b, c) + solve(a - 1, b - 1, c) + solve(a - 1, b, c - 1) - solve(a - 1, b - 1, c - 1);
-    return dp[a][b][c];
+    if (a < b && b < c)
+        return dp[a][b][c] = solve(a, b, c - 1) + solve(a, b - 1, c - 1) - solve(a, b - 1, c);
+    return dp[a][b][c] = solve(a - 1, b, c) + solve(a - 1, b - 1, c) + solve(a - 1, b, c - 1) - solve(a - 1, b - 1, c - 1);
 }
 
 int main() {
