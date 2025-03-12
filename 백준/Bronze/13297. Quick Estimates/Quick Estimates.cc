249c9debec1a8a849edc21cf0514b21d90234e97
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-03-12 23:11:29 +0900

    [Bronze IV] Title: Quick Estimates, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.cc
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+int main() {
+    fastio; string s; int n; cin >> n;
+    while (n--) {cin >> s; cout << s.size() << '\n';}
+}
