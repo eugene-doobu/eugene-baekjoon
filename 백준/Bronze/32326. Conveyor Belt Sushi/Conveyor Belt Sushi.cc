1cd7e28c2996cda5fcf169fe01118fc6a79e0bf8
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-03-14 18:39:04 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+int main() {
+    fastio; int a, b, c; cin >> a >> b >> c;
+    cout << a * 3 + b * 4 + c * 5;
+}
