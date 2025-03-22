232b950f216cf01d22fa8ad5a453f50c8772e66a
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-03-22 15:38:34 +0900

    [Bronze V] Title: ブラックジャック (Blackjack), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+int main() {
+    fastio; int a, b, c; cin >> a >> b >> c; 
+    if (a + b + c <= 21) cout << 1;
+    else cout << 0;
+}
