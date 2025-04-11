2c6a4d3363c921f1a83841673057925c9f57bbb7
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-04-11 14:02:44 +0900

    [Bronze III] Title: Don’t pass me the ball!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6768. Don’t pass me the ball！/Don’t pass me the ball！.cc
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+int main() {
+    fastio; int i; cin >> i;
+	cout << ((i - 1) * (i - 2) * (i - 3) / 6);
+}
