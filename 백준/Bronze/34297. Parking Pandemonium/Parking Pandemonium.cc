7242b8dc967dd8aec8074da12c94b47598bbd083
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-09-10 20:19:04 +0900

    [Bronze V] Title: Parking Pandemonium, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34297. Parking Pandemonium/Parking Pandemonium.cc
@@ -0,0 +1,10 @@
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#include <iostream>
+using namespace std;
+int main() {
+    fastio;
+    int m, k, c, ans;
+    cin >> m >> k >> c;
+    ans = m * c;
+    cout << ans << "\n";
+}
