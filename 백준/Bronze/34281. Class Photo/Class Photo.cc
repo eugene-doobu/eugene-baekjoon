2d968f9d19eae67c0c5fb1787b93440e1949f5c5
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-09-10 20:19:47 +0900

    [Bronze V] Title: Class Photo, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34281. Class Photo/Class Photo.cc
@@ -0,0 +1,10 @@
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#include <iostream>
+using namespace std;
+int main() {
+    fastio;
+    int w, l, ans;
+    cin >> w >> l;
+    ans = w * l;
+    cout << ans << "\n";
+}
