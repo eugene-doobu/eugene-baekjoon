1519ccc7b3bc5b49a676d3e1e21ae034bbae547b
eugene-hong <58686228+eugene-doobu@users.noreply.github.com>
2025-09-13 03:28:05 +0900

    [Bronze IV] Title: Left Beehind, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24768. Left Beehind/Left Beehind.cc
@@ -0,0 +1,14 @@
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#include <iostream>
+using namespace std;
+int main() {
+    fastio; int x, y;
+    while (true) {
+        cin >> x >> y;
+        if (x == 0 && y == 0) break;
+        if (x + y == 13) cout << "Never speak again.\n";
+        else if (y > x) cout << "Left beehind.\n";
+        else if (x > y) cout << "To the convention.\n";
+        else cout << "Undecided.\n";
+    }
+}
